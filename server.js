55336601a1fbdfe9f6118a3d8570106d067a4a0a
Nicholas Roberson <nroberson7712@gmail.com>
2022-05-05 19:41:02 -0600

    need to fix some code and add styling

--- server.js
+++ server.js
@@ -1,37 +1,58 @@
-const express = require('express');
-const fs = require('fs');
-const pug = require('pug');
+const express = require("express");
+const bodyParser = require("body-parser");
+const pug = require("pug");
+const fs = require("fs");
 const port = 3000;
 const server = express();
-server.use(express.json());
-const featureJSON = require('./public/features.json');
+const featureJSON = require("./public/features.json");
+const destinations = require("./public/destinations.json");
+server.use(bodyParser.json());
+server.use(
+	bodyParser.urlencoded({
+		extended: true,
+	})
+);
 
-server.get('/',(req,res,next)=>{
-    const template = pug.compileFile('public/templates/title.pug')({});
-    res.end(template);
+server.get("/", (req, res, next) => {
+	const template = pug.compileFile("public/templates/title.pug")({destinations: destinations.destinations});
+	res.end(template);
 });
 
-server.get('/features',(req,res,next)=>{
-    const template = pug.compileFile('public/templates/features.pug')({'featureJSON': featureJSON.features});
-    res.end(template);
+server.get("/features", (req, res, next) => {
+	const template = pug.compileFile("public/templates/features.pug")({
+		featureJSON: featureJSON.features,
+        destinations: destinations.destinations
+	});
+	res.end(template);
 });
 
-server.get('/orders', (req,res,next)=>{
-    const template = pug.compileFile('public/templates/order.pug')({});
-    res.end(template);
+server.get("/orders", (req, res, next) => {
+	const template = pug.compileFile("public/templates/order.pug")({destinations: destinations.destinations});
+	res.end(template);
 });
 
-server.post('/orderIntake', (req,res,next)=>{
-    const template = pug.compileFile('./public/templates/orderComplete.pug')({});
-    res.end(template);
-    let data = res.json();
-    fs.writeFile('./public/output/order.txt', data, (req,res)=>{
-        
-    });
-    res.redirect('/orders');
+server.post("/orderIntake", (req, res, next) => {
+	const template = pug.compileFile("public/templates/orderComplete.pug")({});
+	res.send(template);
+	fs.open("./public/output/order.txt", "r", function (err, fd) {
+		if (err) {
+			fs.writeFile("./public/order.txt", "", function (err) {
+				if (err) console.error(err);
+				console.log("file created");
+			});
+		}
+		fs.writeFileSync(
+			"./public/order.txt",
+			JSON.stringify(req.body),
+			(error) => {
+				console.log(res);
+				if (error) throw error;
+			}
+		);
+	});
 });
 
-server.listen(port,(error)=>{
-    if(error) throw error;
-    console.log(`server running on ${port}`);
+server.listen(port, (error) => {
+	if (error) throw error;
+	console.log(`server running on ${port}`);
 });
